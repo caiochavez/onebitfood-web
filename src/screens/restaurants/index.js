c72ca02e04872ca68a3a8cb4dc57201f69fa9743
caiochavez <caaiochavez@gmail.com>
2019-05-01 14:27:40 -0300

    Lista de Restaurantes

---
+++ src/screens/restaurants/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListRestaurants from '../../components/list_restaurants'
+
+const Restaurants = () => (
+  <>
+    <ListRestaurants />
+  </>
+)
+ 
+export default Restaurants
